f77cadf118694f5786f3aedbe8062e3fd6feb9ea
Arif Sami SAHIN <arifsamisahin@outlook.com>
2019-05-08 20:37:28 +0300

    Updates

---
+++ precache-manifest.42ed6dd38c0f8afa8c9b9484cadb5ef7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "817cb491a316a7a09ccef1708fe82059",
+    "url": "/contactmanager/index.html"
+  },
+  {
+    "revision": "ac7a54737cdbcb91ec72",
+    "url": "/contactmanager/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "ac7a54737cdbcb91ec72",
+    "url": "/contactmanager/static/js/2.7a916b8d.chunk.js"
+  },
+  {
+    "revision": "7ceea6219a43b078225c",
+    "url": "/contactmanager/static/js/main.0341c7f0.chunk.js"
+  },
+  {
+    "revision": "a9b80a0b0e722e0d3664",
+    "url": "/contactmanager/static/js/runtime~main.884ecd02.js"
+  }
+]);
